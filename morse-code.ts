13693c849520323bd4e0a7b0178b501e11c06f61
Ilya Vikharev <iv660@yandex.ru>
2021-11-13 14:47:23 +0300

    Добавлено: поясняющая константа

--- morse-code.ts
+++ morse-code.ts
@@ -1,9 +1,6 @@
-
-
-
-
-
 import { MORSE_CODE } from './preloaded';
 export function decodeMorse(morseCode: string): string {
-  return morseCode.split('   ').map(word => word.trim().split(' ').map(letter => MORSE_CODE[letter]).join('')).join(' ').trim();
+  const WORD_SEPARATOR = '   ';
+  
+  return morseCode.split(WORD_SEPARATOR).map(word => word.trim().split(' ').map(letter => MORSE_CODE[letter]).join('')).join(' ').trim();
 }
