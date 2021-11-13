9e0033ae806b0e9267b80e084aa73fa50e6b2041
Ilya Vikharev <iv660@yandex.ru>
2021-11-13 15:01:38 +0300

    Добавлено: временная переменная words

--- morse-code.ts
+++ morse-code.ts
@@ -2,5 +2,7 @@ import { MORSE_CODE } from './preloaded';
 export function decodeMorse(morseCode: string): string {
   const WORD_SEPARATOR = '   ';
   
-  return morseCode.split(WORD_SEPARATOR).map(word => word.trim().split(' ').map(letter => MORSE_CODE[letter]).join('')).join(' ').trim();
+  const words: string[] = morseCode.split(WORD_SEPARATOR);
+  
+  return words.map(word => word.trim().split(' ').map(letter => MORSE_CODE[letter]).join('')).join(' ').trim();
 }
