785737f0bf3c978058e085cbc96ddba4c9a5567d
Ilya Vikharev <iv660@yandex.ru>
2021-11-13 15:55:17 +0300

    Добавлено: поясняющая константа LETTER_SEPARATOR

--- morse-code.ts
+++ morse-code.ts
@@ -1,13 +1,14 @@
 import { MORSE_CODE } from './preloaded';
 export function decodeMorse(morseCode: string): string {
   const WORD_SEPARATOR = '   ';
+  const LETTER_SEPARATOR = ' ';
   
   const words: string[] = morseCode.split(WORD_SEPARATOR);
   
   const decodedWords: string[] = [];
   for (const word of words) {
     const unpaddedWord = word.trim();
-    const letters = unpaddedWord.split(' ');
+    const letters = unpaddedWord.split(LETTER_SEPARATOR);
     
     const decodedLetters: string[] = [];
     for (const letter of letters) {
