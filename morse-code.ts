da20f78dbced6156984cd752f3d7022758afd4d8
Ilya Vikharev <iv660@yandex.ru>
2021-11-14 14:39:12 +0300

    Добавлено: класс EncodedMessage

--- morse-code.ts
+++ morse-code.ts
@@ -2,53 +2,69 @@ import { MORSE_CODE } from './preloaded';
 
 
 export function decodeMorse(morseCode: string): string {
-  const encodedWords = breakIntoWords(morseCode);
+  const encodedMessage = new EncodedMessage(morseCode);
   
-  const decodedWords: string[] = [];
-  for (const encodedWord of encodedWords) {
-    const decodedWord = decodeWord(encodedWord);
-    decodedWords.push(decodedWord);
+  return encodedMessage.decodedText;
+}
+
+// =========================================================
+
+class EncodedMessage {
+  
+  constructor(private encodedText: string) {
+    
   }
   
-  const decodedMessage = makeDecodedMessage(decodedWords);
+  public get decodedText(): string {
   
-  return decodedMessage;
-}
+    const encodedWords = this.breakIntoWords(this.encodedText);
 
+    const decodedWords: string[] = [];
+    for (const encodedWord of encodedWords) {
+      const decodedWord = this.decodeWord(encodedWord);
+      decodedWords.push(decodedWord);
+    }
 
-function breakIntoWords(encodedMessage: string): string[] {
-  const WORD_SEPARATOR = '   ';
-  
-  return encodedMessage.split(WORD_SEPARATOR);
-} 
+    const decodedMessage = this.makeDecodedMessage(decodedWords);
 
-
-function decodeWord(encodedWord: string): string {
-  const LETTER_SEPARATOR = ' ';
-  
-  const unpaddedWord = encodedWord.trim();
-  const encodedLetters = unpaddedWord.split(LETTER_SEPARATOR);
-
-  const decodedLetters: string[] = [];
-  for (const encodedLetter of encodedLetters) {
-    const decodedLetter = decodeLetter(encodedLetter);
-    decodedLetters.push(decodedLetter);
+    return decodedMessage;
   }
-  
-  const decodedWord = decodedLetters.join('');
-  
-  return decodedWord;
-}
 
 
-function decodeLetter(encodedLetter: string): string {
-  return MORSE_CODE[encodedLetter];
-}
+  private breakIntoWords(encodedMessage: string): string[] {
+    const WORD_SEPARATOR = '   ';
+
+    return encodedMessage.split(WORD_SEPARATOR);
+  } 
 
 
-function makeDecodedMessage(decodedWords: string[]): string {
-  const decodedMessage = decodedWords.join(' ');
-  const unpaddedDecodedMessage = decodedMessage.trim();
-  
-  return unpaddedDecodedMessage;
+  private decodeWord(encodedWord: string): string {
+    const LETTER_SEPARATOR = ' ';
+
+    const unpaddedWord = encodedWord.trim();
+    const encodedLetters = unpaddedWord.split(LETTER_SEPARATOR);
+
+    const decodedLetters: string[] = [];
+    for (const encodedLetter of encodedLetters) {
+      const decodedLetter = this.decodeLetter(encodedLetter);
+      decodedLetters.push(decodedLetter);
+    }
+
+    const decodedWord = decodedLetters.join('');
+
+    return decodedWord;
+  }
+
+
+  private decodeLetter(encodedLetter: string): string {
+    return MORSE_CODE[encodedLetter];
+  }
+
+
+  private makeDecodedMessage(decodedWords: string[]): string {
+    const decodedMessage = decodedWords.join(' ');
+    const unpaddedDecodedMessage = decodedMessage.trim();
+
+    return unpaddedDecodedMessage;
+  }
 }
