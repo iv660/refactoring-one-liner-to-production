c9e86e2fe4d1c8ee0a5f186e777422fd31d59bec
Ilya Vikharev <iv660@yandex.ru>
2021-11-14 11:23:05 +0300

    Извлечены функции для отдельных действий

--- morse-code.ts
+++ morse-code.ts
@@ -2,26 +2,51 @@ import { MORSE_CODE } from './preloaded';
 
 
 export function decodeMorse(morseCode: string): string {
-  const WORD_SEPARATOR = '   ';
-  const LETTER_SEPARATOR = ' ';
-  
-  const encodedWords: string[] = morseCode.split(WORD_SEPARATOR);
+  const encodedWords = breakIntoWords(morseCode);
   
   const decodedWords: string[] = [];
   for (const encodedWord of encodedWords) {
-    const unpaddedWord = encodedWord.trim();
-    const encodedLetters = unpaddedWord.split(LETTER_SEPARATOR);
-    
-    const decodedLetters: string[] = [];
-    for (const encodedLetter of encodedLetters) {
-      const decodedLetter = MORSE_CODE[encodedLetter];
-      decodedLetters.push(decodedLetter);
-    }
-    
-    const decodedWord = decodedLetters.join('');
+    const decodedWord = decodeWord(encodedWord);
     decodedWords.push(decodedWord);
   }
   
+  const decodedMessage = makeDecodedMessage(decodedWords);
+  
+  return decodedMessage;
+}
+
+
+function breakIntoWords(encodedMessage: string): string[] {
+  const WORD_SEPARATOR = '   ';
+  
+  return encodedMessage.split(WORD_SEPARATOR);
+} 
+
+
+function decodeWord(encodedWord: string): string {
+  const LETTER_SEPARATOR = ' ';
+  
+  const unpaddedWord = encodedWord.trim();
+  const encodedLetters = unpaddedWord.split(LETTER_SEPARATOR);
+
+  const decodedLetters: string[] = [];
+  for (const encodedLetter of encodedLetters) {
+    const decodedLetter = decodeLetter(encodedLetter);
+    decodedLetters.push(decodedLetter);
+  }
+  
+  const decodedWord = decodedLetters.join('');
+  
+  return decodedWord;
+}
+
+
+function decodeLetter(encodedLetter: string): string {
+  return MORSE_CODE[encodedLetter];
+}
+
+
+function makeDecodedMessage(decodedWords: string[]): string {
   const decodedMessage = decodedWords.join(' ');
   const unpaddedDecodedMessage = decodedMessage.trim();
   
